3ae7a387e47da761f79a37be9d3796f87d693b45
Diego Santos (5531) <diego.santos@quiver.local>
2024-06-25 14:12:49 -0300

    incluindo o mmc

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Digite dois números para chegarmos ao MMC e MDC');
+process.stdin.once("data", function (data){
+    let num1 = Number(data.toString().trim());
+    process.stdin.once("data", function (data){
+        let num2 = Number(data.toString().trim());
+
+        function calcularMMC(a, b) {
+                let maior = Math.max(a, b);
+                let mmc = maior;
+
+        while (mmc % a != 0 || mmc % b != 0){
+            mmc += maior;
+        }
+        return mmc
+        }
+
+        let mmc = calcularMMC(num1, num2);
+        console.log(`O MMC dos dois números é ${mmc}`)
+        process.exit();
+    })
+})
