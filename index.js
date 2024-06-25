df5d63bef3e16b48d506f3c7e2c229c8a8bed002
Luanhts <luanh3nrique08@gmail.com>
2024-06-25 18:47:06 -0300

    função do mdc

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ process.stdin.once("data", function (data){
     process.stdin.once("data", function (data){
         let num2 = Number(data.toString().trim());
 
+        
         function calcularMMC(a, b) {
-                let maior = Math.max(a, b);
-                let mmc = maior;
-
-        while (mmc % a != 0 || mmc % b != 0){
-            mmc += maior;
+            let maior = Math.max(a, b);
+            let mmc = maior;
+            
+            while (mmc % a != 0 || mmc % b != 0){
+                mmc += maior;
+            }
+            return mmc
         }
-        return mmc
+        function calcMdc(a,b){
+            let mmc = calcularMMC(a,b)
+            let mdc = (a*b) / mmc
+            return mdc
         }
-
         let mmc = calcularMMC(num1, num2);
-        console.log(`O MMC dos dois números é ${mmc}`)
+        let mdc = calcMdc(num1, num2)
+        
+        console.log(`O MMC dos dois números é ${mmc} e o MDC é ${mdc}`)
         process.exit();
     })
 })
